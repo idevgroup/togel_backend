e4ab39347225e146762ea9b2befc5cd61938710a
kov <kov.lyhuoth@outlook.com>
2019-07-09 08:27:20 +0700

    lyhuoth

--- app/Libraries/CoreConfig.php
+++ app/Libraries/CoreConfig.php
@@ -12,5 +12,5 @@ define('_DIRE_IS_MONTH',date('m-Y'));
 define('_IMG_GENSETLOGO_THUM_W',220);
 define('_IMG_GENSETLOGO_THUM_H',220);
 
-define('_IMG_GENSETICON_THUM_W',220);
-define('_IMG_GENSETICON_THUM_H',220);
+define('_IMG_GENSETICON_THUM_W',32);
+define('_IMG_GENSETICON_THUM_H',45);
